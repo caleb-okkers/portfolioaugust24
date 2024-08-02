05dae76eb0dbb48e87c2ea90897f1c1c9364f0b9
Caleb Okkers <okkerscaleb@gmail.com>
2024-08-02 22:12:28 +0200

    home

--- src/components/HomeSection.vue
+++ src/components/HomeSection.vue
@@ -3,9 +3,10 @@
     <div class="row d-flex justify-content-center align-items-center">
         <div class="col d-flex justify-content-end">
             <div class="intro-div pb-5">
-                <h1 class="intro">Hi I'm Caleb Okkers</h1>
+                <h1 class="intro">I'M CALEB OKKERS</h1>
                 <h3 class="title">Web Developer</h3>
-                <h3 class="intro-text">I build pixel perfect projects for the web.</h3>
+                <p class="intro-text">I design exquisite interfaces, build pixel perfect projects and I inspire innovation.</p>
+                <button class="about-button mx-auto">About me</button>
             </div>
         </div>
         <div class="col me-auto">
@@ -25,15 +26,38 @@ export default {
 
 .profile {
     aspect-ratio: 1;
-    width: 120%;
+    width: 110%;
+    margin-right: -10rem;
 }
 
 .intro-div {
     color: #fff;
+    margin-right: 0;
+    margin-left: 8rem;
 }
 
 .intro {
     font-size: 4rem;
+}
+
+h1 {
+    font-size: 4rem !important;
+}
+
+h3 {
+    font-size: 2rem;
+}
+
+p {
+    font-size: 1.2rem;
+}
+
+.about-button {
+    margin: 0 auto;
+    border: 2px solid #fff;
+    padding: 1rem;
+    background: #001a34;
+    color: #fff;
 }
 
 </style>
