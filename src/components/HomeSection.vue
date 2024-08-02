eccd021cdf78e6583522d3965c87092c49072365
Caleb Okkers <okkerscaleb@gmail.com>
2024-08-02 20:34:24 +0200

    img

--- src/components/HomeSection.vue
+++ src/components/HomeSection.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container d-flex justify-content-center align-items-center">
     <div class="row d-flex justify-content-center align-items-center">
-        <div class="col">
-            <img src="https://github.com/caleb-okkers/portfolioaugust24/blob/master/src/assets/caleb-whiteshirt.png?raw=true" alt="">
-        </div>
         <div class="col"></div>
+        <div class="col">
+            <img class="profile" src="https://github.com/caleb-okkers/portfolioaugust24/blob/master/src/assets/caleb-whiteshirt.png?raw=true" alt="">
+        </div>
     </div>
   </div>
 </template>
@@ -15,6 +15,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+.profile {
+    aspect-ratio: 1;
+    width: 130%;
+}
 
 </style>
