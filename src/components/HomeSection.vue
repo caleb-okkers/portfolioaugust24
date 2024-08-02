5d42731bebe12b7cb479511dc0dee230f77a0eed
Caleb Okkers <okkerscaleb@gmail.com>
2024-08-02 21:07:20 +0200

    img

--- src/components/HomeSection.vue
+++ src/components/HomeSection.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="container d-flex justify-content-center align-items-center">
-    <div class="row d-flex justify-content-center">
-        <div class="col"></div>
+    <div class="row d-flex justify-content-center align-items-center">
+        <div class="col d-flex justify-content-end">
+            <div class="intro-div pb-5">
+                <h1 class="intro">Hi I'm Caleb Okkers</h1>
+                <h3 class="title">Web Developer</h3>
+                <h3 class="intro-text">I build pixel perfect projects for the web.</h3>
+            </div>
+        </div>
         <div class="col me-auto">
-            <img class="profile" src="https://github.com/caleb-okkers/portfolioaugust24/blob/master/src/assets/caleb-whiteshirt-duotoneblue.png?raw=true" alt="">
+            <img class="profile" src="https://github.com/caleb-okkers/portfolioaugust24/blob/master/src/assets/caleb-whiteshirt-duotoneclassic.png?raw=true" alt="">
         </div>
     </div>
   </div>
@@ -19,7 +25,15 @@ export default {
 
 .profile {
     aspect-ratio: 1;
-    width: 130%;
+    width: 120%;
+}
+
+.intro-div {
+    color: #fff;
+}
+
+.intro {
+    font-size: 4rem;
 }
 
 </style>
