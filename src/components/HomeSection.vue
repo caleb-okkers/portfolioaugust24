eb9db328f77505f3ddc979903d13f93207dfac26
Caleb Okkers <okkerscaleb@gmail.com>
2024-08-02 20:44:56 +0200

    img

--- src/components/HomeSection.vue
+++ src/components/HomeSection.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container d-flex justify-content-center align-items-center">
-    <div class="row d-flex justify-content-center align-items-center">
+    <div class="row d-flex justify-content-center">
         <div class="col"></div>
-        <div class="col">
-            <img class="profile" src="https://github.com/caleb-okkers/portfolioaugust24/blob/master/src/assets/caleb-whiteshirt.png?raw=true" alt="">
+        <div class="col me-auto">
+            <img class="profile" src="https://github.com/caleb-okkers/portfolioaugust24/blob/master/src/assets/caleb-whiteshirt-duotoneblue.png?raw=true" alt="">
         </div>
     </div>
   </div>
