ee73ab7ddeb91b98e4285d1af4049af5758753aa
Caleb Okkers <okkerscaleb@gmail.com>
2024-08-03 11:57:36 +0200

    home

--- src/components/HomeSection.vue
+++ src/components/HomeSection.vue
@@ -3,15 +3,16 @@
     <div class="row d-flex justify-content-center align-items-center">
         <div class="col d-flex justify-content-end">
             <div class="intro-div pb-5">
-                <p>Hi, my name is</p>
+                <p class="intro-text">Hi, my name is</p>
                 <h1 class="intro">CALEB OKKERS.</h1>
                 <h3 class="title">Web Developer</h3>
                 <p class="intro-text">I design and build pixel perfect projects that inspire innovation and provides satisfying digital experiences.</p>
-                <button class="about-button mx-auto">About me</button>
+                <button class="about-button btn mx-auto"><span class="text-light">About me</span></button>
+                <button class="btn sort m-2 " type="button" id="sort"><span class="text-light">My work</span></button>
             </div>
         </div>
         <div class="col me-auto">
-            <img class="profile" src="https://github.com/caleb-okkers/portfolioaugust24/blob/master/src/assets/caleb-whiteshirt-duotoneclassic.png?raw=true" alt="">
+            <img class="profile" src="https://github.com/caleb-okkers/portfolioaugust24/blob/master/src/assets/6.png?raw=true" alt="">
         </div>
     </div>
   </div>
@@ -47,6 +48,7 @@ h1 {
   font-optical-sizing: auto;
   font-weight: 500;
   font-style: normal;
+  color: #a69f94;
 }
 
 .title {
@@ -65,12 +67,101 @@ p {
     font-style: normal;
 }
 
-.about-button {
+/* .about-button {
     margin: 0 auto;
     border: 2px solid #fff;
     padding: 1rem;
     background: #001a34;
     color: #fff;
+} */
+
+
+.about-button {
+  position: relative;
+  overflow: hidden;
+  background-color: #001a34 !important;
+  color: #fff !important;
+  border-color: #001a34 !important;
+  text-align: center !important;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition: color 0.5s ease;
+  border-radius: 0 !important;
+  z-index: 1;
+  will-change: color;
+  font-size: 1.18rem;
+    font-family: "Montserrat", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 300;
+    font-style: normal;
 }
+
+.btn {
+  position: relative;
+  overflow: hidden;
+  background-color: #001a34 !important;
+  color: #fff !important;
+  border-color: #fff !important;
+  text-align: center !important;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition: color 0.5s ease;
+  border-radius: 0 !important;
+  z-index: 1;
+  will-change: color; 
+  font-size: 1.18rem;
+    font-family: "Montserrat", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 300;
+    font-style: normal;
+}
+
+.btn::before,
+.about-button::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #575757;
+  z-index: 0;
+  transition: transform 0.5s ease;
+  transform: scaleX(0);
+  transform-origin: left;
+  backface-visibility: hidden; 
+  will-change: transform; 
+}
+
+.btn:hover::before,
+.about-button:hover::before {
+  transform: scaleX(1);
+}
+
+.btn:hover,
+.about-button:hover::before {
+  color: #001a34 !important; 
+}
+
+.btn span,
+.about-button span {
+  position: relative;
+  z-index: 2;
+  backface-visibility: hidden; 
+  will-change: color; 
+  color: #fff;
+  
+}
+
+.btn:hover span,
+.about-button:hover span {
+  position: relative;
+  z-index: 2;
+  backface-visibility: hidden; 
+  will-change: color; 
+  color: #fff !important;
+  
+}
+
 
 </style>
